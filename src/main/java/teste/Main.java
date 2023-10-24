a65b06bb088f1aa7cb341481bc31300decde2358
Pedro Duarte <p.fmarket1996@gmail.com>
2023-10-23 21:48:51 -0300

    Conexão com banco de dados

--- src/main/java/teste/Main.java
+++ src/main/java/teste/Main.java
@@ -1,7 +1,22 @@
 package teste;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("agendaCrud");
+
+        EntityManager em = emf.createEntityManager();
+
+        if(em != null){
+            System.out.println("Conexão Estabelecida com sucesso");
+        }else {
+            System.out.println("Falha ao estabelecer conexão");
+        }
+
+        em.clear();
+        emf.close();
     }
 }
